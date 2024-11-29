24c7eba815f83ff550d2b7e4ecf6c54c331d8455
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2024-11-30 00:22:52 +0300

    basket bug fix

--- CourseSales.Basket.Api/Features/Baskets/Dtos/BasketDto.cs
+++ CourseSales.Basket.Api/Features/Baskets/Dtos/BasketDto.cs
@@ -2,12 +2,22 @@
 
 namespace CourseSales.Basket.Api.Features.Baskets.Dtos
 {
-    public record BasketDto(
+    public record BasketDto
 
-        List<BasketItemDto> BasketItems
-    )
+    
     {
-        [JsonIgnore] public Guid UserId { get; set; }
+        [JsonIgnore] public Guid UserId { get; init; }
+        public List<BasketItemDto> BasketItems { get; init; } = new();
+        public BasketDto(Guid userId,List<BasketItemDto> basketItem)
+        {
+            UserId = userId;
+            BasketItems = basketItem;
+        }
+
+        public BasketDto()
+        {
+            
+        }
        
     }
 
