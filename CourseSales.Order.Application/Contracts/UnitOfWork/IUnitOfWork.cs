68c307b1f0975f7935c6f3fb130025778f17fa5a
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-03-13 23:37:37 +0300

    unitofwork add

---
+++ CourseSales.Order.Application/Contracts/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSales.Order.Application.Contracts.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken=default);
+        void BeginTransactionAsync();
+        Task CommitTransactionAsync(CancellationToken cancellationToken=default);
+
+    }
+}
---
+++ CourseSales.Order.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using CourseSales.Order.Application.Contracts.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSales.Order.Persistence.UnitOfWork
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        public void BeginTransactionAsync()
+        {
+            context.Database.BeginTransaction();
+        }
+
+        public Task CommitTransactionAsync(CancellationToken cancellationToken=default)
+        {
+            return context.Database.CommitTransactionAsync(cancellationToken);
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken=default)
+        {
+            return context.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}
