4ccd04a03774b76ff8d28d2d977c56867ca7953d
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-08-02 13:44:32 +0300

    gateway settings ok

--- CourseSales.Gateway/Program.cs
+++ CourseSales.Gateway/Program.cs
@@ -1,7 +1,14 @@
+using CourseSales.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
+
 var app = builder.Build();
 app.MapReverseProxy();
 app.MapGet("/", () => "Hello World!");
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
